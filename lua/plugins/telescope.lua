2f8f6c9347c81af5f6a294c66745baa572b098b9
Stivi <stivi.ndoni@softup.co>
2025-06-01 12:04:52 +0200

    Added remap for buffer list <leader>bl

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,6 +6,7 @@ return {
   config = function()
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
+    vim.keymap.set('n', '<leader>bl', builtin.buffers, { desc = 'Telescope list open buffers' }) 
     vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find git files' })
     vim.keymap.set('n', '<leader>ps', function()
       builtin.grep_string({ search = vim.fn.input("Grep > ") })
