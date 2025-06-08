ddaa456b0a5a0c8e6dbb12c91947c6c2fef871b0
noxsteve <stivi.ndoni@gmail.com>
2025-06-08 22:34:05 +0200

    Added mappings for twin characters

--- lua/nox/remap.lua
+++ lua/nox/remap.lua
@@ -1,3 +1,13 @@
 vim.g.mapleader = " "
+
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+vim.keymap.set("i", "'", "''<left>")
+vim.keymap.set("i", "\"", "\"\"<left>")
+vim.keymap.set("i", "(", "()<left>")
+vim.keymap.set("i", "[", "[]<left>")
+vim.keymap.set("i", "{", "{}<left>")
+vim.keymap.set("i", "{;", "{};<left><left>")
+vim.keymap.set("i", "/*", "/**/<left><left>")
+
