690e377c15ac910c548552bf8751aeea0e30d438
Stivi <stivi.ndoni@softup.co>
2025-06-09 20:30:55 +0200

    Show hidden files by default

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -6,7 +6,16 @@ return {
     opts = {
       keymaps = {
         ['<C-p>'] = false,
-      }
+      },
+      view_options = {
+        show_hidden = true,
+        is_hidden_file = function(name, bufnr)
+          return vim.startswith(name, '.')
+        end,
+        is_always_hidden = function(name, bufnr)
+          return false
+        end,
+      },
     },
     -- Optional dependencies
     dependencies = { { "echasnovski/mini.icons", opts = {} } },
