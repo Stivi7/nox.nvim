4cc59642be285bfc7233245c894328717b24fffa
noxsteve <stivi.ndoni@gmail.com>
2025-06-11 00:32:00 +0200

    Update plugins. Disabled remap

--- lua/nox/remap.lua
+++ lua/nox/remap.lua
@@ -9,5 +9,5 @@ vim.keymap.set("i", "(", "()<left>")
 vim.keymap.set("i", "[", "[]<left>")
 vim.keymap.set("i", "{", "{}<left>")
 vim.keymap.set("i", "{;", "{};<left><left>")
-vim.keymap.set("i", "/*", "/**/<left><left>")
+-- vim.keymap.set("i", "/*", "/**/<left><left>")
 
